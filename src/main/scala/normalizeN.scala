4a918165d341b2a94e8056aacede6bf776ce4c6b
Andrew Waterman <waterman@eecs.berkeley.edu>
2012-11-17 06:50:26 -0800

    simplify and improve QoR of Normalize

--- src/main/scala/normalizeN.scala
+++ src/main/scala/normalizeN.scala
@@ -5,16 +5,9 @@ import Node._
 
 object Normalize
 {
-  def doit(width: Int, pos: Int, in: Bits, dist: Bits): Pair[Bits,Bits] = {
-    if (pos == width)
-      (in, dist)
-    else {
-      val shift = in(width-1,pos) === Bits(0)
-      doit(width, width - (width-pos)/2, Mux(shift, Cat(in(pos-1,0), Bits(0, width-pos)), in), if (dist == null) shift else Cat(dist, shift))
-    }
-  }
   def apply(in: Bits) = {
     require((in.getWidth & (in.getWidth-1)) == 0)
-    doit(in.getWidth, in.getWidth/2, in, null)
+    val shift = ~Log2(in, in.getWidth)
+    (in << shift, shift)
   }
 }
