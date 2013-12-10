fbc6d66a418a7367a79ac42e947f2f9a47d70760
Andrew Waterman <waterman@eecs.berkeley.edu>
2013-12-09 20:24:21 -0800

    Make fmaTest support f32 or f64

--- src/main/scala/fmaTest.scala
+++ src/main/scala/fmaTest.scala
@@ -21,15 +21,16 @@ class FMA(sigWidth: Int, expWidth: Int)  extends Module {
   io.out := recodedFloatNToFloatN(fma.io.out, sigWidth, expWidth)
 }
 
-class FMATests(c: FMA) extends Tester(c, Array(c.io)) {
-  def d2i(x: Double) = java.lang.Double.doubleToLongBits(x)
-  def i2d(x: Long) = java.lang.Double.longBitsToDouble(x)
-  def gen = util.Random.nextDouble
+class FMATests(c: FMA, s: Int) extends Tester(c, Array(c.io)) {
+  require(s == 32 || s == 64)
+  def i2d(x: Long) =
+    if (s == 32) java.lang.Float.intBitsToFloat(x.toInt)
+    else java.lang.Double.longBitsToDouble(x)
   val vars = new collection.mutable.HashMap[Node, Node]()
 
   def canonicalize(x: BigInt) = {
     if (i2d(x.toLong).isNaN)
-      (BigInt(1) << 64) - 1
+      (BigInt(1) << s) - 1
     else
       x
   }
@@ -47,7 +48,7 @@ class FMATests(c: FMA) extends Tester(c, Array(c.io)) {
 
   def testAll: Boolean = {
     val logger = ProcessLogger(testOne, _ => ())
-    Seq("bash", "-c", "testfloat_gen -rnear_even -n 6133248 f64_mulAdd | head -n10000") ! logger
+    Seq("bash", "-c", "testfloat_gen -rnear_even -n 6133248 f"+s+"_mulAdd | head -n10000") ! logger
     ok
   }
   defTests {
@@ -57,7 +58,9 @@ class FMATests(c: FMA) extends Tester(c, Array(c.io)) {
 
 object FMATest {
   def main(args: Array[String]): Unit = {
-    chiselMainTest(args ++ Array("--compile", "--test",  "--genHarness", "--debug", "--vcd"),
-                   () => Module(new FMA(52, 12))) { c => new FMATests(c) }
+    //chiselMainTest(args ++ Array("--compile", "--test",  "--genHarness"),
+    //               () => Module(new FMA(52, 12))) { c => new FMATests(c, 64) }
+    chiselMainTest(args ++ Array("--compile", "--test",  "--genHarness"),
+                   () => Module(new FMA(23, 9))) { c => new FMATests(c, 32) }
   }
 }
