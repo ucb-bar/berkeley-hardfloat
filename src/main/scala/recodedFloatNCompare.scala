d461a1f931cb52e91efc87dc3675454135ab4eac
Andrew Waterman <waterman@cs.berkeley.edu>
2015-09-16 15:12:19 -0700

    Fix comparison between differently recoded infinities

--- src/main/scala/recodedFloatNCompare.scala
+++ src/main/scala/recodedFloatNCompare.scala
@@ -20,22 +20,25 @@ class recodedFloatNCompare(SIG_WIDTH: Int, EXP_WIDTH: Int) extends Module {
   val expA = io.a(SIG_WIDTH+EXP_WIDTH-1, SIG_WIDTH)
   val sigA = io.a(SIG_WIDTH-1,0)
   val codeA = expA(EXP_WIDTH-1, EXP_WIDTH-3)
-  val isZeroA = !codeA.orR
-  val isNaNA = codeA.andR
+  val isZeroA = codeA === UInt(0)
+  val isInfA = codeA === UInt(6)
+  val isNaNA = codeA === UInt(7)
   val isSignalingNaNA = isNaNA && !sigA(SIG_WIDTH-1)
 
   val signB = io.b(SIG_WIDTH+EXP_WIDTH)
   val expB = io.b(SIG_WIDTH+EXP_WIDTH-1,SIG_WIDTH)
   val sigB = io.b(SIG_WIDTH-1,0)
   val codeB = expB(EXP_WIDTH-1, EXP_WIDTH-3)
-  val isZeroB = !codeB.orR
-  val isNaNB = codeB.andR
+  val isZeroB = codeB === UInt(0)
+  val isInfB = codeB === UInt(6)
+  val isNaNB = codeB === UInt(7)
   val isSignalingNaNB = isNaNB && !sigB(SIG_WIDTH-1)
 
   val signEqual = signA === signB
   val expEqual = expA === expB
-  val magEqual = expEqual && sigA === sigB
-  val magLess = expA < expB || expEqual && sigA < sigB
+  val magsInf = isInfA && isInfB
+  val magEqual = (expEqual && sigA === sigB) || magsInf
+  val magLess = (expA < expB || (expEqual && sigA < sigB)) && !magsInf
 
   io.a_eq_b_invalid := isSignalingNaNA || isSignalingNaNB
   io.a_lt_b_invalid := isNaNA || isNaNB
