b827f54fc2883250b9ab29b2410cc1cc2613f4e9
John Hauser <jhauser@eecs.berkeley.edu>
2016-11-18 14:02:14 -0800

    Restore the HardFloat package name to lowercase, to conform to Java conventions.

--- src/main/scala/ValExec_MulAddRecFN.scala
+++ src/main/scala/ValExec_MulAddRecFN.scala
@@ -35,7 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =============================================================================*/
 
-package HardFloat
+package hardfloat
 
 import Chisel._
 
