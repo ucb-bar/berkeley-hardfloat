c62888aefd65dd89fd8e0db61ab935d408f359f5
Tianrui Wei <tianrui@tianruiwei.com>
2022-11-07 16:13:13 -0800

    fix: import fix

--- src/main/scala/DivSqrtRecFN_small.scala
+++ src/main/scala/DivSqrtRecFN_small.scala
@@ -187,7 +187,7 @@ package hardfloat
 
 import chisel3._
 import chisel3.util._
-
+import consts._
 /*----------------------------------------------------------------------------
 | Computes a division or square root for floating-point in recoded form.
 | Multiple clock cycles are needed for each division or square-root operation,
